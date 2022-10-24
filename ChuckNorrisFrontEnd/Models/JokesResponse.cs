575f088971334e588cccde25d6a2f48a780df9da
Simuzeche Kaluwa <Simuzeche Kaluwa@DESKTOP-2FK2OT4>
2022-10-24 11:41:50 +0200

    Add project files.

---
+++ ChuckNorrisFrontEnd/Models/JokesResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace ChuckNorrisFrontEnd.Models
+{
+    public class JokesResponse
+    {
+        public string category { get; set; } = string.Empty;
+        public string setup { get; set; } = string.Empty;
+        public string delivery { get; set; } = string.Empty;
+
+
+    }
+}
